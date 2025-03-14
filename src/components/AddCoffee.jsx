46a0f2efd5ce2243cdb44fccd2ec2eb176f5dd15
SheaikhNazib <sheaikh.rahman@northsouth.edu>
2025-03-15 02:21:20 +0600

    addCoffee, updateCoffee updated

--- src/components/AddCoffee.jsx
+++ src/components/AddCoffee.jsx
@@ -4,6 +4,8 @@ import Swal from 'sweetalert2'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faRotateLeft } from '@fortawesome/free-solid-svg-icons';
 import gif2 from '../../images/gif/gif2.gif'
+import Header from './Header';
+import Footer from './Footer';
 
 const AddCoffee = () => {
 
@@ -48,89 +50,93 @@ const AddCoffee = () => {
     }
 
     return (
-        <div className='lg:w-3/4 mx-auto p-20 my-10' style={{ backgroundColor: '#f4f3f0' }}>
+        <>
+        <Header></Header>
+            <div className='lg:w-3/4 mx-auto p-20 my-10' style={{ backgroundColor: '#f4f3f0' }}>
 
-            <div className='flex justify-between'>
-                <Link to='/' className='text-2xl my-auto' style={{ fontFamily: 'Sour Gummy, sans-serif', textShadow: '2px 2px 4px rgba(0, 0, 0, 0.5)' }}><FontAwesomeIcon className='me-2 my-auto' icon={faRotateLeft} size="md" />Back to home</Link>
+                <div className='flex justify-between'>
+                    <Link to='/' className='text-2xl my-auto' style={{ fontFamily: 'Sour Gummy, sans-serif', textShadow: '2px 2px 4px rgba(0, 0, 0, 0.5)' }}><FontAwesomeIcon className='me-2 my-auto' icon={faRotateLeft} size="md" />Back to home</Link>
 
-                <img src={gif2} className='w-86 h-44' alt="" />
+                    <img src={gif2} className='w-86 h-44' alt="" />
+                </div>
+
+
+                <div className="text-center p-10">
+
+                    <h1 className="font-bold text-3xl" style={{ color: '#331a15', fontFamily: 'Sour Gummy, sans-serif', textShadow: '2px 2px 4px rgba(0, 0, 0, 0.5)' }}>Add New Coffee!</h1>
+
+                    <p className="py-6">
+                        It is a long established fact that a reader will be distraceted by the readable content of a page when looking at its layout. The point of using Lorem Ipsum is that it has a more-or-less normal distribution of letters, as opposed to using Content here.
+                    </p>
+                </div>
+                <div className="card bg-base-100 w-full shrink-0 shadow-2xl">
+
+                    <form onSubmit={handleAddCoffee} className="card-body">
+
+                        {/* form first row */}
+                        <div className='flex flex-col lg:flex-row gap-5'>
+                            <div className="form-control flex-1">
+                                <label className="label">
+                                    <span className="label-text my-1 font-bold">Name</span>
+                                </label><br />
+                                <input type="text" name='name' placeholder="Coffee name" className="input input-bordered" required />
+                            </div>
+                            <div className="form-control flex-1">
+                                <label className="label">
+                                    <span className="label-text my-1 font-bold">Chef</span>
+                                </label><br />
+                                <input type="text" name='chef' placeholder="Chef name" className="input input-bordered" required />
+                            </div>
+                        </div>
+
+                        {/* form second row */}
+                        <div className='flex flex-col lg:flex-row gap-5'>
+                            <div className="form-control flex-1">
+                                <label className="label">
+                                    <span className="label-text my-1 font-bold">Supplier</span>
+                                </label> <br />
+                                <input type="text" name='supplier' placeholder="Coffee supplier" className="input input-bordered" required />
+                            </div>
+                            <div className="form-control flex-1">
+                                <label className="label">
+                                    <span className="label-text my-1 font-bold">Taste</span>
+                                </label> <br />
+                                <input type="text" name='taste' placeholder="Taste name" className="input input-bordered" required />
+                            </div>
+                        </div>
+
+                        {/* form third row */}
+                        <div className='flex flex-col lg:flex-row gap-5'>
+                            <div className="form-control flex-1">
+                                <label className="label">
+                                    <span className="label-text my-1 font-bold">Price</span>
+                                </label> <br />
+                                <input type="text" name='price' placeholder="Coffee price" className="input input-bordered" required />
+                            </div>
+                            <div className="form-control flex-1">
+                                <label className="label">
+                                    <span className="label-text my-1 font-bold">Details</span>
+                                </label> <br />
+                                <input type="text" name='details' placeholder="Coffee Details" className="input input-bordered" required />
+                            </div>
+                        </div>
+
+
+                        <div className="form-control">
+                            <label className="label">
+                                <span className="label-text my-1 font-bold">Photo URL</span>
+                            </label> <br />
+                            <input type="text" name='photo' placeholder="Photo url" className="input input-bordered" required />
+
+                        </div>
+                        <div className="form-control mt-6">
+                            <button className="btn w-full font-thin btn-hover hover:text-white" style={{ backgroundColor: '#d2b48c', fontFamily: 'Sour Gummy, sans-serif', textShadow: '2px 2px 4px rgba(0, 0, 0, 0.5)' }}>Add Coffee</button>
+                        </div>
+                    </form>
+                </div>
             </div>
-
-
-            <div className="text-center p-10">
-
-                <h1 className="font-bold text-3xl" style={{ color: '#331a15', fontFamily: 'Sour Gummy, sans-serif', textShadow: '2px 2px 4px rgba(0, 0, 0, 0.5)' }}>Add New Coffee!</h1>
-
-                <p className="py-6">
-                    It is a long established fact that a reader will be distraceted by the readable content of a page when looking at its layout. The point of using Lorem Ipsum is that it has a more-or-less normal distribution of letters, as opposed to using Content here.
-                </p>
-            </div>
-            <div className="card bg-base-100 w-full shrink-0 shadow-2xl">
-
-                <form onSubmit={handleAddCoffee} className="card-body">
-                    
-                    {/* form first row */}
-                    <div className='flex flex-col lg:flex-row gap-5'>
-                        <div className="form-control flex-1">
-                            <label className="label">
-                                <span className="label-text my-1 font-bold">Name</span>
-                            </label><br />
-                            <input type="text" name='name' placeholder="Coffee name" className="input input-bordered" required />
-                        </div>
-                        <div className="form-control flex-1">
-                            <label className="label">
-                                <span className="label-text my-1 font-bold">Chef</span>
-                            </label><br />
-                            <input type="text" name='chef' placeholder="Chef name" className="input input-bordered" required />
-                        </div>
-                    </div>
-
-                    {/* form second row */}
-                    <div className='flex flex-col lg:flex-row gap-5'>
-                        <div className="form-control flex-1">
-                            <label className="label">
-                                <span className="label-text my-1 font-bold">Supplier</span>
-                            </label> <br />
-                            <input type="text" name='supplier' placeholder="Coffee supplier" className="input input-bordered" required />
-                        </div>
-                        <div className="form-control flex-1">
-                            <label className="label">
-                                <span className="label-text my-1 font-bold">Taste</span>
-                            </label> <br />
-                            <input type="text" name='taste' placeholder="Taste name" className="input input-bordered" required />
-                        </div>
-                    </div>
-
-                    {/* form third row */}
-                    <div className='flex flex-col lg:flex-row gap-5'>
-                        <div className="form-control flex-1">
-                            <label className="label">
-                                <span className="label-text my-1 font-bold">Price</span>
-                            </label> <br />
-                            <input type="text" name='price' placeholder="Coffee price" className="input input-bordered" required />
-                        </div>
-                        <div className="form-control flex-1">
-                            <label className="label">
-                                <span className="label-text my-1 font-bold">Details</span>
-                            </label> <br />
-                            <input type="text" name='details' placeholder="Coffee Details" className="input input-bordered" required />
-                        </div>
-                    </div>
-
-
-                    <div className="form-control">
-                        <label className="label">
-                            <span className="label-text my-1 font-bold">Photo URL</span>
-                        </label> <br />
-                        <input type="text" name='photo' placeholder="Photo url" className="input input-bordered" required />
-
-                    </div>
-                    <div className="form-control mt-6">
-                        <button className="btn w-full font-thin btn-hover hover:text-white" style={{ backgroundColor: '#d2b48c', fontFamily: 'Sour Gummy, sans-serif', textShadow: '2px 2px 4px rgba(0, 0, 0, 0.5)' }}>Add Coffee</button>
-                    </div>
-                </form>
-            </div>
-        </div>
+            <Footer></Footer>
+        </>
     );
 };
 
--- src/components/UpdateCoffee.jsx
+++ src/components/UpdateCoffee.jsx
@@ -3,6 +3,8 @@ import { Link, useLoaderData, useNavigate } from 'react-router-dom';
 import Swal from 'sweetalert2';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faRotateLeft } from '@fortawesome/free-solid-svg-icons';
+import Footer from './Footer';
+import Header from './Header';
 
 const UpdateCoffee = () => {
     const coffee = useLoaderData();
@@ -50,79 +52,86 @@ const UpdateCoffee = () => {
     };
 
     return (
-        <div className='lg:w-3/4 mx-auto'>
-            <div className="p-10">
-                <Link to='/' className='text-2xl my-auto' style={{ fontFamily: 'Sour Gummy, sans-serif', textShadow: '2px 2px 4px rgba(0, 0, 0, 0.5)' }}>
-                    <FontAwesomeIcon className='me-2 my-auto' icon={faRotateLeft} size="md" />Back to home
-                </Link>
-                <h1 className="font-bold text-3xl text-center mt-7" style={{ color: '#331a15', fontFamily: 'Sour Gummy, sans-serif', textShadow: '2px 2px 4px rgba(0, 0, 0, 0.5)' }}>
-                    Update Existing Coffee Details
-                </h1>
-                <p className="py-6">
-                    It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout. The point of using Lorem Ipsum is that it has a more-or-less normal distribution of letters, as opposed to using Content here.
-                </p>
+        <>
+            <Header></Header>
+
+            <div className="min-h-screen flex flex-col">
+                <div className='lg:w-3/4 mx-auto flex-grow'>
+                    <div className="p-10">
+                        <Link to='/' className='text-2xl my-auto' style={{ fontFamily: 'Sour Gummy, sans-serif', textShadow: '2px 2px 4px rgba(0, 0, 0, 0.5)' }}>
+                            <FontAwesomeIcon className='me-2 my-auto' icon={faRotateLeft} size="md" />Back to home
+                        </Link>
+                        <h1 className="font-bold text-3xl text-center mt-7" style={{ color: '#331a15', fontFamily: 'Sour Gummy, sans-serif', textShadow: '2px 2px 4px rgba(0, 0, 0, 0.5)' }}>
+                            Update Existing Coffee Details
+                        </h1>
+                        <p className="py-6 text-center">
+                            It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout. The point of using Lorem Ipsum is that it has a more-or-less normal distribution of letters, as opposed to using Content here.
+                        </p>
+                    </div>
+                    <div className="card bg-base-100 w-full shrink-0 shadow-2xl">
+                        <form onSubmit={handleUpdateCoffee} className="card-body">
+                            {/* form first row */}
+                            <div className='flex flex-col lg:flex-row gap-5'>
+                                <div className="form-control flex-1">
+                                    <label className="label">
+                                        <span className="label-text my-1 font-bold">Name</span>
+                                    </label><br />
+                                    <input type="text" name='name' defaultValue={name} placeholder="Coffee name" className="input input-bordered" required />
+                                </div>
+                                <div className="form-control flex-1">
+                                    <label className="label">
+                                        <span className="label-text my-1 font-bold">Chef</span>
+                                    </label><br />
+                                    <input type="text" name='chef' defaultValue={chef} placeholder="Chef name" className="input input-bordered" required />
+                                </div>
+                            </div>
+                            {/* form second row */}
+                            <div className='flex flex-col lg:flex-row gap-5'>
+                                <div className="form-control flex-1">
+                                    <label className="label">
+                                        <span className="label-text my-1 font-bold">Supplier</span>
+                                    </label> <br />
+                                    <input type="text" name='supplier' defaultValue={supplier} placeholder="Coffee supplier" className="input input-bordered" required />
+                                </div>
+                                <div className="form-control flex-1">
+                                    <label className="label">
+                                        <span className="label-text my-1 font-bold">Taste</span>
+                                    </label> <br />
+                                    <input type="text" name='taste' defaultValue={taste} placeholder="Taste name" className="input input-bordered" required />
+                                </div>
+                            </div>
+                            {/* form third row */}
+                            <div className='flex flex-col lg:flex-row gap-5'>
+                                <div className="form-control flex-1">
+                                    <label className="label">
+                                        <span className="label-text my-1 font-bold">Price</span>
+                                    </label> <br />
+                                    <input type="text" name='price' defaultValue={price} placeholder="Coffee price" className="input input-bordered" required />
+                                </div>
+                                <div className="form-control flex-1">
+                                    <label className="label">
+                                        <span className="label-text my-1 font-bold">Details</span>
+                                    </label> <br />
+                                    <input type="text" name='details' defaultValue={details} placeholder="Coffee Details" className="input input-bordered" required />
+                                </div>
+                            </div>
+                            <div className="form-control">
+                                <label className="label">
+                                    <span className="label-text my-1 font-bold">Photo URL</span>
+                                </label> <br />
+                                <input type="text" name='photo' defaultValue={photo} placeholder="Photo url" className="input input-bordered" required />
+                            </div>
+                            <div className="form-control mt-6">
+                                <button className="btn w-full font-thin btn-hover hover:text-white" style={{ backgroundColor: '#d2b48c', fontFamily: 'Sour Gummy, sans-serif', textShadow: '2px 2px 4px rgba(0, 0, 0, 0.5)' }}>
+                                    Update Coffee Details
+                                </button>
+                            </div>
+                        </form>
+                    </div>
+                </div>
             </div>
-            <div className="card bg-base-100 w-full shrink-0 shadow-2xl">
-                <form onSubmit={handleUpdateCoffee} className="card-body">
-                    {/* form first row */}
-                    <div className='flex flex-col lg:flex-row gap-5'>
-                        <div className="form-control flex-1">
-                            <label className="label">
-                                <span className="label-text my-1 font-bold">Name</span>
-                            </label><br />
-                            <input type="text" name='name' defaultValue={name} placeholder="Coffee name" className="input input-bordered" required />
-                        </div>
-                        <div className="form-control flex-1">
-                            <label className="label">
-                                <span className="label-text my-1 font-bold">Chef</span>
-                            </label><br />
-                            <input type="text" name='chef' defaultValue={chef} placeholder="Chef name" className="input input-bordered" required />
-                        </div>
-                    </div>
-                    {/* form second row */}
-                    <div className='flex flex-col lg:flex-row gap-5'>
-                        <div className="form-control flex-1">
-                            <label className="label">
-                                <span className="label-text my-1 font-bold">Supplier</span>
-                            </label> <br />
-                            <input type="text" name='supplier' defaultValue={supplier} placeholder="Coffee supplier" className="input input-bordered" required />
-                        </div>
-                        <div className="form-control flex-1">
-                            <label className="label">
-                                <span className="label-text my-1 font-bold">Taste</span>
-                            </label> <br />
-                            <input type="text" name='taste' defaultValue={taste} placeholder="Taste name" className="input input-bordered" required />
-                        </div>
-                    </div>
-                    {/* form third row */}
-                    <div className='flex flex-col lg:flex-row gap-5'>
-                        <div className="form-control flex-1">
-                            <label className="label">
-                                <span className="label-text my-1 font-bold">Price</span>
-                            </label> <br />
-                            <input type="text" name='price' defaultValue={price} placeholder="Coffee price" className="input input-bordered" required />
-                        </div>
-                        <div className="form-control flex-1">
-                            <label className="label">
-                                <span className="label-text my-1 font-bold">Details</span>
-                            </label> <br />
-                            <input type="text" name='details' defaultValue={details} placeholder="Coffee Details" className="input input-bordered" required />
-                        </div>
-                    </div>
-                    <div className="form-control">
-                        <label className="label">
-                            <span className="label-text my-1 font-bold">Photo URL</span>
-                        </label> <br />
-                        <input type="text" name='photo' defaultValue={photo} placeholder="Photo url" className="input input-bordered" required />
-                    </div>
-                    <div className="form-control mt-6">
-                        <button className="btn w-full font-thin btn-hover hover:text-white" style={{ backgroundColor: '#d2b48c', fontFamily: 'Sour Gummy, sans-serif', textShadow: '2px 2px 4px rgba(0, 0, 0, 0.5)' }}>
-                            Update Coffee Details
-                        </button>
-                    </div>
-                </form>
-            </div>
-        </div>
+            {/* <Footer></Footer> */}
+        </>
     );
 };
 
